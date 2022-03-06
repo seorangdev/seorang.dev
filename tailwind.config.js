2d5ab137463d339ce7561305386081ad35da7ce7
Fajar Ru <kzofajar@gmail.com>
2022-03-06 11:30:59 +0700

    Add light color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,21 @@ module.exports = {
           200: '#444144',
           400: '#222122',
           600: '#1a191a',
+          800: '#1a181a',
         },
         'dark-primary': '#c6a976',
         'dark-secondary': '#b9b6b0',
+        'sun': {
+          100: '#f9f8f4', // card
+          200: '#f6f3ef', // background
+          300: '#f0ece4', // highlight
+          400: '#ece7de', // sidebar
+          500: '#c89e3d', // primary color, link
+          600: '#e0d7c6', // border
+          700: '#af9d81', // primary button, subtitle
+          800: '#7b6a4f', // heading
+          900: '#41311d', // text
+        }
       }
     },
   },
