8325df879c51cc21eb224c0d29b91e9c14d3edf3
Fajar Ru <kzofajar@gmail.com>
2022-03-16 17:13:01 +0700

    Small homepage update & cleanup unused styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,8 @@ module.exports = {
     fontFamily: {
       sans: ['Karla', 'sans-serif']
     },
-
     extend: {
       colors: {
-        'dark-base': {
-          200: '#444144',
-          400: '#222122',
-          600: '#1a191a',
-          800: '#1a181a',
-        },
-        'dark-primary': '#c6a976',
-        'dark-secondary': '#b9b6b0',
         'sun': {
           100: '#f9f8f4', // card
           200: '#f6f3ef', // background
